e42ddd88a29f5c57710e828a14fcd86ad213feb5
sachinNishalka <nishalkasachin@gmail.com>
2024-02-25 15:45:49 +0530

    finished the design of the home page

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,10 +1,14 @@
 import "package:cashapp/components/BottomMainNavigationBar.dart";
 import "package:cashapp/components/CustomizedAppBar.dart";
+import 'package:cashapp/components/PrimaryContainer.dart';
+import 'package:cashapp/components/PrimaryTextComponent.dart';
+import 'package:cashapp/components/SecondaryContainer.dart';
 import "package:flutter/material.dart";
 import 'package:cashapp/components/GetSectionData.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'dart:ui' as ui;
 import 'package:cashapp/data/myExpenses.dart';
+import 'package:cashapp/components/PieChart.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({super.key});
@@ -41,44 +45,71 @@ class _HomeScreenState extends State<HomeScreen> {
                 padding: EdgeInsets.only(right: 25.0, left: 25.0, bottom: 25.0),
                 child: Column(
                   children: [
-                    SfCircularChart(
-                      onCreateShader: (ChartShaderDetails chartShaderDetails) {
-                        return ui.Gradient.radial(
-                          chartShaderDetails.outerRect.center,
-                          chartShaderDetails.outerRect.right -
-                              chartShaderDetails.outerRect.center.dx,
-                          <Color>[
-                            Color(0xFFB3A142),
-                            Color(0xFFA9A647),
-                            Color(0xFFA4AA4B),
-                            Color(0xFF9CAE4F),
-                            Color(0xFF99B861),
-                            Color(0xFF9BBB67), // blue sky
-                            Color(0xFFA9C681),
-                          ],
-                          <double>[0.2, 0.4, 0.6, 0.8, 10.0, 12.0, 14.0],
-                        );
-                      },
-                      series: <CircularSeries>[
-                        PieSeries<MyExpenses, String>(
-                          explode: true,
-                          radius: "100%",
-                          dataLabelSettings: DataLabelSettings(
-                            isVisible: true,
-                            textStyle:
-                                TextStyle(fontSize: 14.0, color: Colors.white),
+                    PieChart(chartData: _chartData),
+                    PrimaryContainer(
+                      componentWidgets: Column(
+                        children: [
+                          Row(
+                            children: [
+                              PrimaryTextComponent(
+                                textStatement: "Save",
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ],
                           ),
-                          strokeColor: Color(0xFFCCAD5E),
-                          dataSource: _chartData,
-                          xValueMapper: (MyExpenses data, _) =>
-                              data.expenseName,
-                          yValueMapper: (MyExpenses data, _) =>
-                              data.expenceValue,
-                          dataLabelMapper: (MyExpenses data, _) =>
-                              "${data.expenseName}",
-                        ),
-                      ],
-                    )
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.baseline,
+                            textBaseline: TextBaseline.alphabetic,
+                            children: [
+                              PrimaryTextComponent(
+                                textStatement: "Rs",
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.w600,
+                              ),
+                              SizedBox(
+                                width: 5.0,
+                              ),
+                              PrimaryTextComponent(
+                                textStatement: "3,000",
+                                fontSize: 40.0,
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    SecondaryContainer(
+                      componentWidgets: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              PrimaryTextComponent(
+                                textStatement: "Your expenses \n reduced by",
+                                fontWeight: FontWeight.w500,
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  PrimaryTextComponent(
+                                    textStatement: "10%",
+                                    fontSize: 40.0,
+                                  ),
+                                  PrimaryTextComponent(
+                                    textStatement: "This month",
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
